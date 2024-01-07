f0c51343cb747d496093922701346a7845e3a718
Evgeniy <moytlf@gmail.com>
2024-01-07 23:58:20 +0300

    Removed unnecessary pv and pvc for mysql conf

--- mysync8.0/start.sh
+++ mysync8.0/start.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
+export SERVER_ID=$(($(echo $POD_NAME | grep -Eo "[0-9]*") + 1000))
+
 ( echo "cat <<EOF" ; cat /etc/configs/mysync.yaml ) | sh > /etc/mysync.yaml
+( echo "cat <<EOF" ; cat /etc/configs/server.cnf ) | sh > /etc/mysql/conf.d/server.cnf
 
 if [ ! -f /tmp/usedspace ]; then
     echo 10 > /tmp/usedspace
 fi
+
 if [ ! -f /tmp/readonly ]; then
     echo "false" > /tmp/readonly
 fi
 
-if [ ! -f /tmp/cluster_initialized ]; then
-    rm -rf /var/lib/mysql/*
+if [ -z "$(ls -A /var/lib/mysql)" ]; then
     mysqld --initialize --init-file=/etc/configs/init.sql
-    echo 'true' > /tmp/cluster_initialized
+    chown mysql:mysql -R /var/lib/mysql
 fi
-
-chown mysql:mysql -R /var/lib/mysql
 
 service mysql start
 
-mysql -proot -e "SELECT 1;"
-while [ $? != 0 ]
-do
-mysql -proot -e "SELECT 1;"
-done
-
 while :
 do
-if ! pgrep -x "mysync" > /dev/null
-then
-	service mysync-init start
-fi
-
 if ! pgrep -x "mysqld" > /dev/null
 then
 	service mysql start
+fi
+
+if ! pgrep -x "mysync" > /dev/null
+then
+	mysync >> /var/log/mysync.log 2>&1 &
 fi
 done
