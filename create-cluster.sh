7555a28d54c4e313601e1b5fc1a5fc2f540ffae1
noname0443 <noname342341@gmail.com>
2024-01-18 00:27:34 +0300

    Minikube exposed and moved pvc

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,16 +1,22 @@
 #!/bin/zsh
+
+set -e
+
 if [ -z "$1" ]; then
 	echo "ERROR: enter name of cluster"
+  exit 1;
 fi
 
 if [ -z "$2" ]; then
 	echo "ERROR: enter replica count"
+  exit 1;
 fi
 
 if [ -z "$3" ]; then
 	echo "ERROR: enter mysql password"
+  exit 1;
 fi
 
 helm install $1 mysql-chart --atomic --set replicaCount=$2 --set mysql.root_pass=$3
 port=$(kubectl get svc $1 -n services -n services -o jsonpath='{.spec.ports[].nodePort}')
-kubectl port-forward $1 -n services $port:3306 --address 192.168.1.65 &
+kubectl port-forward $1 -n services $port:3306 --address 192.168.1.65 1>/dev/null 2>&1 &
--- start-cloud.sh
+++ start-cloud.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 
+set -e
+
 minikube delete
 export NO_PROXY=localhost,127.0.0.1
 minikube start --container-runtime=docker --network-plugin=cni --cni=calico
+minikube cp ./resolv.conf /etc/resolv.conf
 minikube addons enable registry
 kubectl create namespace mysql
 kubectl create namespace services
