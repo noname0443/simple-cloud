d9496e6129c6e8fffe21e94d244a5eccf1011b94
noname0443 <noname342341@gmail.com>
2024-01-22 00:18:20 +0300

    Add port to ClusterInfo struct

--- internal/kubernetes/kubernetes.go
+++ internal/kubernetes/kubernetes.go
@@ -15,6 +15,7 @@ type PodState struct {
 
 type ClusterInfo struct {
 	PodStates []PodState
+	Port      int
 }
 
 type Cluster interface {
@@ -40,45 +41,74 @@ func (cls *K8sCluster) IsExist() (bool, error) {
 	return true, nil
 }
 
-func ParseClusterInfo(rawClusterInfo []byte) (error, ClusterInfo) {
+func ParsePort(rawPortInfo []byte) (int, error) {
+	m := make(map[string]interface{})
+	err := json.Unmarshal(rawPortInfo, &m)
+	if err != nil {
+		return -1, err
+	}
+	spec, ok := m["spec"].(map[string]interface{})
+	if !ok {
+		return -2, err
+	}
+
+	ports, ok := spec["ports"].([]interface{})
+	if !ok || len(ports) == 0 {
+		return -3, err
+	}
+
+	firstPort, ok := ports[0].(map[string]interface{})
+	if !ok {
+		return -4, err
+	}
+
+	floatNodePort, ok := firstPort["nodePort"].(float64)
+	nodePort := int(floatNodePort)
+	if !ok {
+		return -5, err
+	}
+	return nodePort, err
+}
+
+func ParsePodStates(rawClusterInfo []byte) ([]PodState, error) {
 	m := make(map[string]interface{})
 	err := json.Unmarshal(rawClusterInfo, &m)
 	if err != nil {
-		return err, ClusterInfo{}
+		return []PodState{}, err
 	}
 	items, ok := m["items"].([]interface{})
 	if !ok {
-		return err, ClusterInfo{}
+		return []PodState{}, err
 	}
 
-	clusterInfo := ClusterInfo{}
+	podStates := make([]PodState, len(items))
 	for i := 0; i < len(items); i++ {
 		item, ok := items[i].(map[string]interface{})
 		if !ok {
-			return err, ClusterInfo{}
+			return []PodState{}, err
 		}
 		status, ok := item["status"].(map[string]interface{})
 		if !ok {
-			return err, ClusterInfo{}
+			return []PodState{}, err
 		}
 		phase, ok := status["phase"].(string)
 		if !ok {
-			return err, ClusterInfo{}
+			return []PodState{}, err
 		}
 		metadata, ok := item["metadata"].(map[string]interface{})
 		if !ok {
-			return err, ClusterInfo{}
+			return []PodState{}, err
 		}
 		name, ok := metadata["name"].(string)
 		if !ok {
-			return err, ClusterInfo{}
+			return []PodState{}, err
 		}
-		clusterInfo.PodStates = append(clusterInfo.PodStates, PodState{
+		podStates[i] = PodState{
 			State:   phase,
 			PodName: name,
-		})
+		}
 	}
-	return err, clusterInfo
+	return podStates, err
 }
 
 func (cls *K8sCluster) GetState() (ClusterInfo, error) {
@@ -96,8 +126,27 @@ func (cls *K8sCluster) GetState() (ClusterInfo, error) {
 	if err != nil {
 		return ClusterInfo{}, err
 	}
-	err, clusterInfo := ParseClusterInfo(output)
-	return clusterInfo, err
+	podStates, err := ParsePodStates(output)
+
+	output, err = exec.Command(
+		"kubectl",
+		"get",
+		"svc",
+		"-n",
+		"services",
+		cls.Name,
+		"-o",
+		"json",
+	).Output()
+	if err != nil {
+		return ClusterInfo{}, err
+	}
+	port, err := ParsePort(output)
+
+	return ClusterInfo{
+		PodStates: podStates,
+		Port:      port,
+	}, err
 }
 
 func (cls *K8sCluster) Create() error {
